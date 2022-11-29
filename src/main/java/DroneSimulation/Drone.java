31d1ec084f5d4644c8ed374829969ce5abf97a8d
Henry <HenryKhoo1313@gmail.com>
2022-11-29 16:52:50 +0000

    WEEK 1 DONE

--- src/main/java/DroneSimulation/Drone.java
+++ src/main/java/DroneSimulation/Drone.java
@@ -14,6 +14,14 @@ public class Drone {
     public String toString(){
         return "Drone is at "+XPos+" "+YPos;
     }
+    public boolean isHere(int XI, int YI){
+        if((XI==XPos)&&(YI==YPos)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
         Drone d = new Drone(5, 3);		// create drone
         System.out.println(d.toString());	// print where is
--- src/main/java/DroneSimulation/DroneArena.java
+++ src/main/java/DroneSimulation/DroneArena.java
@@ -1,4 +1,52 @@
 package DroneSimulation;
 
+import java.util.*;
 public class DroneArena {
+    Random RG;
+    ArrayList<Drone> DroneList;
+    private int MaxX;
+    private int MaxY;
+    private int MinX;
+    private int MinY;
+    private Drone TDrone;
+    public DroneArena(int MaxXI,int MaxYI){
+        MaxX = MaxXI;
+        MaxY = MaxYI;
+        MinX = 0;
+        MinY = 0;
+        RG = new Random();
+        DroneList = new ArrayList<Drone>();
+    }
+    public void addDrone(){
+        int XPosD,YPosD;
+        do {
+            XPosD = RG.nextInt(MinX+1, MaxX - 1);
+            YPosD = RG.nextInt(MinY+1, MaxX - 1);
+        }
+        while(getDrone(XPosD,YPosD)!=null);
+        TDrone = new Drone(XPosD,YPosD);//Somewhere in the middle of the Map
+        DroneList.add(TDrone);
+    }
+    public Drone getDrone(int XI, int YI){
+        for(int i = 0; i < DroneList.size();i++) {
+            if(DroneList.get(i).isHere(XI,YI)==true){
+                return DroneList.get(i);
+            }
+        }
+        return null;
+    }
+    public String toString(){
+        String Temp = "Arena Size: "+MaxX+" "+ MaxY+"\n";
+        for(int i = 0; i < DroneList.size();i++) {
+            Temp = Temp + DroneList.get(i).toString() + "\n";
+        }
+        return Temp;
+    }
+    public static void main(String[] args) {
+        DroneArena a = new DroneArena(20, 10);	// create drone arena
+        a.addDrone();
+        a.addDrone();
+        System.out.println(a.toString());	// print where is
+    }
+
 }
